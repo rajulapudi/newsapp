e8d5f3bea882927ddd0bce32917d7a032285a337
praneeth <rajulapudip@gmail.com>
2019-06-10 18:41:10 +0530

    initial

---
+++ app.js
@@ -0,0 +1,20 @@
+const NewsAPI = require('newsapi');
+const keys =require('./keys')
+const newsapi = new NewsAPI('014317ccd6ce42e2854ffd72bc0103a2');
+// To query /v2/top-headlines
+// All options passed to topHeadlines are optional, but you need to include at least one of them
+newsapi.v2.topHeadlines({
+  sources: 'bbc-news,the-verge',
+  q: 'bitcoin',
+  category: 'business',
+  language: 'en',
+  country: 'us'
+}).then(response => {
+  console.log(response);
+  /*
+    {
+      status: "ok",
+      articles: [...]
+    }
+  */
+});

65982562f6479b17291e8e1ebbd4a92c4edf776d
praneeth <rajulapudip@gmail.com>
2019-06-11 00:43:55 +0530

    news API integrated

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 const NewsAPI = require('newsapi');
-const keys =require('./keys')
+const keys = require('./keys')
 const newsapi = new NewsAPI('014317ccd6ce42e2854ffd72bc0103a2');
 // To query /v2/top-headlines
 // All options passed to topHeadlines are optional, but you need to include at least one of them
-newsapi.v2.topHeadlines({
-  sources: 'bbc-news,the-verge',
+
+var newsOptions = {
   q: 'bitcoin',
-  category: 'business',
+  sources: 'bbc-news,the-verge',
+  domains: 'bbc.co.uk, techcrunch.com',
   language: 'en',
-  country: 'us'
-}).then(response => {
-  console.log(response);
-  /*
-    {
-      status: "ok",
-      articles: [...]
+  sortBy: 'relevancy',
+  page: 2
+}
+
+newsapi.v2.everything(newsOptions,(err,response)=>{
+  if (err){console.log(err)}
+  else{
+    var htmlelement = '';
+    for(var i=0; i<response.articles.length; i++){
+      htmlelement += '<a class="oflow-hidden pos-relative mb-20 dplay-block" href="'+response.articles[i].url+'">'
+      +'<div class="wh-100x abs-tlr"><img src="'+response.articles[i].urlToImage+'alt=""></div>'
+      +'<div class="ml-120 min-h-100x"><h5><b>'+response.articles[i].title+'</b></h5>'
+     /*  title += '<li>'+response.articles[i].title+'</li>';
+      url += '<a href="'+ response.articles[i].url+'">';
+      console.log(url); */
+      console.log(htmlelement);
     }
-  */
-});
+  }
+})
+
+
